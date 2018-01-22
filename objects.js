df40874d5b66694df2c24ee4876c0e3729f5be59
Peter Babeu <mbeenpete@hotmail.com>
2018-01-22 19:17:41 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,28 @@
+var playlist={'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'};
+
+function updatePlaylist({}, playlist,artistName, songTitle) {
+  playlist.artistName = songTitle;
+  return playlist;
+}
+
+/*
+describe('updatePlaylist(playlist, artistName, songTitle)', () => {
+    before(() => {
+      playlist['Slowdive'] = 'Alison'
+      playlist['My Bloody Valentine'] = 'Sometimes'
+    })
+
+    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
+      updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
+
+      expect(playlist).
+        to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
+    })
+  })
+*/
+
+
+function removeFromPlaylist(playlistObject, artist) {
+  delete playlistObject.artist;
+  return playlistObject;
+}
