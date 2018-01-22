853a0cb8f4895ad0f448eed2336de186665c3d09
Peter Babeu <mbeenpete@hotmail.com>
2018-01-22 20:05:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist={'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   //playlist = playlist.artistName=songTitle; 
-    //playlist.artistName=songTitle
+    playlist[artistName]=songTitle;
   return playlist;
 }
 
